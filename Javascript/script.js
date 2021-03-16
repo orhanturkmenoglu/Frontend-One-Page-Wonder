9ad8a9aa8de16fda699a6b032d0fa920e1b2f9ec
Orhantr13 <orhantrkmn749@gmail.com>
2021-03-16 16:04:47 +0300

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,4 @@
+function islem(){
+    var a=document.getElementById("link").setAttribute("href","https://www.google.com");
+    var a=document.getElementById("link").setAttribute("target","_blank");
+}
